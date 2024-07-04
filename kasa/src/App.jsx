3f906e56225fd17bc9e588478817508de38cb85e
Justine Launay <jlaunay8@outlook.fr>
2024-07-04 15:01:45 +0200

    Routes

--- kasa/src/App.jsx
+++ kasa/src/App.jsx
@@ -1,5 +1,20 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+
 function App() {
-  
+  return (
+    <div className="main-app">
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Accueil />}/>
+        <Route path="/a-propos" element={<A-propos />} />
+        <Route path="/location/:id" element={<Location />} />
+        <Route path="/error" element={<Erreur />} />
+      </Routes>
+
+      <Footer />
+    </div>
+  )
 }
 
 export default App
