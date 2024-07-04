b30991db738fc2a88e36785a24a4e3e1c148439a
Justine Launay <jlaunay8@outlook.fr>
2024-07-04 20:22:05 +0200

    Footer ok

--- kasa/src/components/Footer.jsx
+++ kasa/src/components/Footer.jsx
@@ -1,8 +1,16 @@
+import logoWhite from "../assets/logo/logo-white.png"
+
 function Footer () {
     return (
-        <div>
-            <p>Footer</p>
-        </div>
+        <footer>
+            <div className="logo-white">
+                <img src={logoWhite} alt="Logo de Kasa blanc"/>
+            </div>
+
+            <div className="allRights">
+                <p>© 2020 Kasa. All rights reserved</p>
+            </div>
+        </footer>
     )
 }
 
