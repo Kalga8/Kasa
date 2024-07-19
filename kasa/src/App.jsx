1e750fea414b388e12ad64025b3d0ebdbcdae40d
Justine Launay <jlaunay8@outlook.fr>
2024-07-19 15:31:08 +0200

    path

--- kasa/src/App.jsx
+++ kasa/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />}/>
-        <Route path="/location/:id" element={<Rentals />} />
+        <Route path="/appartement/:id" element={<Rentals />} />
         <Route path="/a-propos" element={<About />} />
         <Route path="*" element={<Error />} />
       </Routes>
