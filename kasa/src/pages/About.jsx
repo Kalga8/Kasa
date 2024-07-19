23c17ac35aab22a4d66f5cdc4a31d2b2ecf99ed4
Justine Launay <jlaunay8@outlook.fr>
2024-07-19 14:33:17 +0200

    Test json

--- kasa/src/pages/About.jsx
+++ kasa/src/pages/About.jsx
@@ -1,6 +1,7 @@
 import Banner from "../components/Banner"
 import background from "../assets/banners/banner-about.png"
 import Collapse from "../components/Collapse"
+import collapseData from "../data/collapse.json"
 
 function About(){
     return (
@@ -10,35 +11,14 @@ function About(){
             />
 
             <div className="collapseContainer">
-                <Collapse
-                    title="Fiabilité"
-                    text="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont
-                    conformes aux logements, et toutes les informations sont régulièrement vérifiées
-                    par nos équipes."
-                />
-
-                <Collapse
-                    title="Respect"
-                    text="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement
-                    discriminatoire ou de perturbation de voisinage entraînera une exclusion de 
-                    notre plateforme."
-                />
-
-                <Collapse
-                    title="Service"
-                    text="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement
-                    discriminatoire ou de perturbation de voisinage entraînera une exclusion de 
-                    notre plateforme."
-                />
-
-                <Collapse
-                    title="Sécurité"
-                    text="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les
-                    voyageurs, chaque logement correspond aux critères de sécurité établis par nos
-                    services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet
-                    à nos équipes de vérifier que les standards sont bien respectés. Nous organisons
-                    également des ateliers sur la sécurité domestique pour nos hôtes."
-                />
+                {collapseData.map((collapse) => (
+                    <Collapse
+                    key={'${collapse.id}'}
+                    title={collapse.title}
+                    text={collapse.text}
+                    />
+                )
+                )}
             </div>
         </div>
     )
