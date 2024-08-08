eb4dd0f0ccc6cbc177eeaa9d5e0ba755f18e2e34
Justine Launay <jlaunay8@outlook.fr>
2024-08-08 14:46:42 +0200

    Media queries rentals

--- kasa/src/pages/Rentals.jsx
+++ kasa/src/pages/Rentals.jsx
@@ -1,57 +1,53 @@
-import { Navigate, useParams } from "react-router-dom"
-import Carousel from "../components/Carousel"
-import rentalsData from "../data/advertisement.json"
-import TitleRentals from "../components/TitleRentals"
-import Host from "../components/Host"
-import Tags from "../components/Tags"
-import Rates from "../components/Rates"
-import Collapse from "../components/Collapse"
+import { Navigate, useParams } from "react-router-dom";
+import Carousel from "../components/Carousel";
+import rentalsData from "../data/advertisement.json";
+import TitleRentals from "../components/TitleRentals";
+import Host from "../components/Host";
+import Tags from "../components/Tags";
+import Rates from "../components/Rates";
+import Collapse from "../components/Collapse";
 
 function Rentals() {
-    const {id}=useParams(); /*Hook récupérant l'id depuis URL*/
-    const selectedRental=rentalsData.find((appartement) =>
-        appartement.id===id);
+  const { id } = useParams(); /*Hook récupérant l'id depuis URL*/
+  const selectedRental = rentalsData.find(
+    (appartement) => appartement.id === id
+  );
 
-    /*Gestion erreur id*/
-    if (!selectedRental) {
-        return <Navigate to="Error"/>
-    }
-    
-    return (
-        <div className="informationsRentals">
-                <Carousel
-                pictures={selectedRental.pictures}
-                />
-            <div className="titleHost">
-                <TitleRentals
-                title={selectedRental.title}
-                location={selectedRental.location}/>
+  /*Gestion erreur id*/
+  if (!selectedRental) {
+    return <Navigate to="Error" />;
+  }
 
-                <Host 
-                name={selectedRental.host.name}
-                picture={selectedRental.host.picture}
-                />
-            </div>
+  return (
+    <div className="informationsRentals">
+      <Carousel pictures={selectedRental.pictures} />
+      <div className="informationsContainer">
+        <div className="titleTags">
+          <TitleRentals
+            title={selectedRental.title}
+            location={selectedRental.location}
+          />
 
-            <div className="tagsRatings">
-                <Tags
-                tags={selectedRental.tags}/>
-
-                <Rates 
-                rating={selectedRental.rating}/>
-            </div>
-
-            <div className="collapseRentals">
-                <Collapse
-                title={"Description"}
-                text={selectedRental.description}/>
-
-                <Collapse
-                title={"Équipements"}
-                text={selectedRental.equipments}/>
-            </div>
+          <Tags tags={selectedRental.tags} />
         </div>
-    )
+
+        <div className="hostRatings">
+          <Host
+            name={selectedRental.host.name}
+            picture={selectedRental.host.picture}
+          />
+
+          <Rates rating={selectedRental.rating} />
+        </div>
+      </div>
+
+      <div className="collapseRentals">
+        <Collapse title={"Description"} text={selectedRental.description} />
+
+        <Collapse title={"Équipements"} text={selectedRental.equipments} />
+      </div>
+    </div>
+  );
 }
 
-export default Rentals
+export default Rentals;
